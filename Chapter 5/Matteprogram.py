490321df5105d541d0f86fc39cbc68f449978de9
BuildTools <unconfigured@null.spigotmc.org>
2021-09-12 23:20:12 +0200

    Added scorecount to exitchoice

--- Chapter 5/Matteprogram.py
+++ Chapter 5/Matteprogram.py
@@ -4,6 +4,9 @@ from time import sleep
 
 operator = ["", "+", "-", "*", "/"]
 
+correct_answer = 0
+wrong_answer = 0
+
 
 def clear():
     if name == "nt":  # for windows
@@ -27,6 +30,9 @@ def menu():
         print("------------------------------------------------------")
         selection = int(input("Enter choice: "))
         if selection == 5:
+            print(
+                f"\nYou had {correct_answer} correct answers and {wrong_answer} wrong answers.\n"
+            )
             exit()
         elif selection in range(1, 5):
             inputcheck = True
@@ -50,11 +56,13 @@ def calculation(number_one, operator, number_two):
 
 
 def main():
+    global wrong_answer
+    global correct_answer
     while True:
         selection = menu()
         number_one = random.randint(1, 10)
         number_two = random.randint(1, 10)
-        if selection == "2":
+        if selection == "2" or "4":
             if number_two > number_one:
                 (number_two, number_one) = (number_one, number_two)
 
@@ -64,11 +72,13 @@ def main():
         answer = calculation(number_one, operator[selection], number_two)
 
         if answer == question:
+            correct_answer += 1
             print("------------------------------------------------------")
             print("That is correct!")
             print("------------------------------------------------------")
 
         elif answer != question:
+            wrong_answer += 1
             print("------------------------------------------------------")
             print(f"Sorry, the correct answer is {answer} Try again!")
             print("------------------------------------------------------\n")
